38f6ce7dc67a3612ef87eb9b5051c3097537f473
Mahmoud Reda <mahmoud.reda@areebgroup.com>
2024-08-17 21:41:33 +0300

    fix displaying logging result of request call

--- composeApp/src/commonMain/kotlin/network/ApiClient.kt
+++ composeApp/src/commonMain/kotlin/network/ApiClient.kt
@@ -1,11 +1,11 @@
 package network
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.compose.viewmodel.dsl.viewModel
@@ -21,7 +21,8 @@ val appModule = module {
 fun createApiClient(): HttpClient {
    return HttpClient {
         install(Logging) {
-            level = LogLevel.ALL
+            level = LogLevel.BODY
+            logger = Logger.DEFAULT
         }
         install(ContentNegotiation) {
             json(
